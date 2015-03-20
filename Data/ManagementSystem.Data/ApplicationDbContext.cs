c05f12514f6a5db97c212e08d32d13a234cb1949
Dr4g0 <d_d0003@yahoo.com>
2015-03-20 14:44:33 +0200

    Refactoring IdentityModels.cs

---
+++ Data/ManagementSystem.Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ManagementSystem.Data.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Linq;
+
+namespace ManagementSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
