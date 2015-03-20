9e3ef38be3a3b3ebaf77c3d6e80fd4607d15c914
Dr4g0 <d_d0003@yahoo.com>
2015-03-20 15:00:01 +0200

    Enabled migrations

---
+++ Data/ManagementSystem.Data/ManagementSystemDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using ManagementSystem.Data.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Linq;
+using ManagementSystem.Data.Migrations;
+
+namespace ManagementSystem.Data
+{
+    public class ManagementSystemDbContext : IdentityDbContext<User>
+    {
+        public ManagementSystemDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ManagementSystemDbContext, Configuration>());
+        }
+
+        public static ManagementSystemDbContext Create()
+        {
+            return new ManagementSystemDbContext();
+        }
+    }
+}
